05489d58180e89af3707c22eb4e37eef0e85e1e2
srilakshmi-darbha <srilakshmi.darbha@thoughtworks.com>
2021-09-21 20:24:06 +0530

    re-commit

--- src/test/java/LoginPageTests.java
+++ src/test/java/LoginPageTests.java
@@ -1,20 +1,12 @@
-import com.qabootcamp.constants.FrameworkConstants;
 import com.qabootcamp.pages.WelcomeBanner;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginPageTests extends BaseTest{
+public class LoginPageTests extends BaseTest {
 
-      @Test
-       public void registerUser(){
-          WelcomeBanner wb = new WelcomeBanner();
-          wb.clickOnDismiss();
-      }
+    @Test
+    public void registerUser() {
+        WelcomeBanner wb = new WelcomeBanner();
+        wb.clickOnDismiss();
+    }
 
 }
